055a4fc102b7e81dba3086721a3c366e46575f8b
Po-jui <chenborui1@gmail.com>
2024-06-03 04:55:48 +0800

    Update home.tsx

--- front-end/src/pages/home/home.tsx
+++ front-end/src/pages/home/home.tsx
@@ -11,7 +11,7 @@ export default function Home(){
             setProjects(response.data)
         }
         findAllProjects();
-    }, []);
+    }, [PROJECTS_API]);
     return (
         <div><ProjectCard projects={projects}/></div>
     )
