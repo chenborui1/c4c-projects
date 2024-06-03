e1aca5c8e64e8a6238f4b3f07b4d96c9e92de80c
Po-jui <chenborui1@gmail.com>
2024-06-03 23:36:44 +0800

    Update home.tsx

--- front-end/src/pages/home/home.tsx
+++ front-end/src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import ProjectCard from '../../components/ProjectCard/project-card.tsx'
-const API_BASE = process.env.SERVER_API_BASE;
+const API_BASE = "https://c4c-projects.onrender.com";
 export default function Home(){
     const [projects, setProjects] = useState([{}])
     const PROJECTS_API = `${API_BASE}/api/projects`;
