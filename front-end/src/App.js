1da2612b5dae9bebb7c2ab50f892deba4cf99b89
Po-jui <chenborui1@gmail.com>
2024-06-05 04:13:43 +0800

    token authentication finished

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,12 @@ import NavBar from "./components/NavBar/NavBar.tsx";
 import Home from "./pages/home/home.tsx";
 import PrivateRoutes from "./PrivateRoutes.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {AuthProvider} from "./AuthContext.js";
 import Admin from "./pages/admin-page/AdminPage.tsx";
 
 function App() {
   return (
     <>
-    <AuthProvider>
+
      <BrowserRouter>
         <Routes>
           <Route path="/" element={
@@ -22,7 +21,7 @@ function App() {
             </Route>
         </Routes>
       </BrowserRouter>
-      </AuthProvider>
+ 
     </>
   );
 }
